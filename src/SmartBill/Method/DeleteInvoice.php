5bfc6b88d0acafca033470f3d380b8149871cea3
Eduard Budacu <eduard.budacu@koding.ro>
2016-05-26 08:21:39 +0300

    added delete, cancel and restore invoice methods

---
+++ src/SmartBill/Method/DeleteInvoice.php
@@ -0,0 +1,46 @@
+<?php
+namespace SmartBill\Method;
+
+/**
+ * Delete invoice
+ *
+ * @author eduard.budacu
+ */
+class DeleteInvoice {
+    
+    protected $cif;
+    protected $seriesname;
+    protected $number;
+    
+    /**
+     * HttpClient response
+     * @var type \Httpful\Response
+     */
+    protected $response;
+    
+    public function __construct($cif, $seriesname, $number) {
+        $this->cif = $cif;
+        $this->seriesname = $seriesname;
+        $this->number = $number;
+    }
+    
+    public function request() {
+        $client = \SmartBill\Client::getHttpClient();
+        $client->method('DELETE');
+        $client->uri($this->_getApiEndpoint());
+        $this->response =  $client->send();
+    }
+    
+    public function isSuccesful() {
+        if(empty($this->response->body->errorText)) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+    
+    protected function _getApiEndpoint() {
+        $baseUri = \SmartBill\Client::BASE_URL . 'invoice';
+        $params = array('cif' => $this->cif, 'seriesname' => $this->seriesname, 'number' => $this->number);
+        return $baseUri . '?' . http_build_query($params);
+    } 
+}
